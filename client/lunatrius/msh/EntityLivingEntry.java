65443fe630e69a2b130398deee9b22d960d18708
Lunatrius <jadran.kotnik@gmail.com>
2012-12-09 06:16:56 +0100

    Remove an unused field.

--- client/lunatrius/msh/EntityLivingEntry.java
+++ client/lunatrius/msh/EntityLivingEntry.java
@@ -4,13 +4,11 @@ import net.minecraft.src.EntityLiving;
 
 public class EntityLivingEntry {
 	public String name = "";
-	public String nameObf = "";
 	public EntityLiving entity = null;
 	public boolean enabled = false;
 
 	public EntityLivingEntry(String name, EntityLiving entity, boolean enabled) {
 		this.name = name;
-		this.nameObf = entity.getClass().getSimpleName();
 		this.entity = entity;
 		this.enabled = enabled;
 	}
